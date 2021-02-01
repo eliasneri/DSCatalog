8ae2708b3baa898a0ce6525d1a951d42f7d6c91a
eliasneri <eliasneri@hotmail.com>
2021-02-01 16:49:17 -0300

    useState, useEffect

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 const App = () => {
-    return <h3>Hello App</h3>
+    const [counter, setCounter] = useState(0);
+    return (
+        <div className="container mt-5">
+           <button
+                className="btn btn-primary mr-5"
+                onClick={() => setCounter(counter + 1) }
+                >
+                +
+            </button>
+           <span>
+               {counter}
+            </span>
+           <button 
+                className="btn btn-primary ml-5"
+                onClick={() => setCounter(counter - 1) }
+                >
+                -
+            </button>
+            <button
+                className="btn btn-primary ml-5"
+                onClick={() => setCounter(0)}
+                >
+                Zerar
+                </button>
+            {counter >= 5 && <h1>O Valor é maior que 5</h1>}
+        </div>
+    );
 }
 
 export default App;
