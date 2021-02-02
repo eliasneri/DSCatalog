ba72393cbca7bffaa0afb3e0b0f052ee9c590ad1
eliasneri <eliasneri@hotmail.com>
2021-02-02 13:30:31 -0300

    Add front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,33 +1,9 @@
-import React, { useState } from 'react';
+import React  from 'react';
 
 
 const App = () => {
-    const [counter, setCounter] = useState(0);
     return (
-        <div className="container mt-5">
-           <button
-                className="btn btn-primary mr-5"
-                onClick={() => setCounter(counter + 1) }
-                >
-                +
-            </button>
-           <span>
-               {counter}
-            </span>
-           <button 
-                className="btn btn-primary ml-5"
-                onClick={() => setCounter(counter - 1) }
-                >
-                -
-            </button>
-            <button
-                className="btn btn-primary ml-5"
-                onClick={() => setCounter(0)}
-                >
-                Zerar
-                </button>
-            {counter >= 5 && <h1>O Valor é maior que 5</h1>}
-        </div>
+    <h1>App</h1>
     );
 }
 
