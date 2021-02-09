1b333ae459d2f805808a02b15f21765d976d2d2c
eliasneri <eliasneri@hotmail.com>
2021-02-09 17:06:29 -0300

    add Routes.. Add Home!

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,13 @@
 import React  from 'react';
-import './core/assets/styles/custom.scss'
+import './core/assets/styles/custom.scss';
 import './app.scss';
-import Navbar from './core/components/navbar';
+
+import Routes from './pages/Routes';
 
 
 const App = () => {
     return (
-        <Navbar />
+       <Routes />
 
     );
 }
---
+++ front-web/src/pages/Home/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './styles.scss';
+import { ReactComponent as MainImage } from '../../core/assets/images/main-image.svg';
+
+const Home = () => (
+  <div className="home-container">
+    <div className="row home-content">
+      <div className="col-6">
+        <h1 className="text-title">Conheça o melhor <br/> Catálogo de Produtos</h1>
+        <p className="text-subtitle">
+          Ajudaremos você a encontrar os melhores <br/> produtos disponíveis no mercado.
+        </p>
+      </div>
+      <div className="col-6">
+        <MainImage className="main-image"/>
+
+      </div>
+    </div>
+      
+  </div>
+);
+
+
+export default Home;
